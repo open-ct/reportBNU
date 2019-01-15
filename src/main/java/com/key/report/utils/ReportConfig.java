fb4a012d1fb7a8002df3feb988cda71dfc5ac980
whalyzh <502593966@qq.com>
2019-01-15 11:30:33 +0800

    调整页边距

--- src/main/java/com/key/report/utils/ReportConfig.java
+++ src/main/java/com/key/report/utils/ReportConfig.java
@@ -9,5 +9,5 @@ public class ReportConfig {
     public static final String HTML_URL = "http://localhost:8080/report/files/reportHtml/";
     public static final String PDF_PATH = "C:/Users/zhuaiballl/Workspaces/MyEclipse 2017/.metadata/.me_tcat85/webapps/report/files/reportPDF/";
     public static final String WK_PATH = "C:/report/wkhtmltopdf/bin/wkhtmltopdf";
-    public static final String WK_PARA = "--disable-smart-shrinking --margin-bottom 0 --margin-left 0 --margin-right 0 --margin-top 0";
+    public static final String WK_PARA = "--disable-smart-shrinking --margin-left 0 --margin-right 0 --margin-top 20 --margin-bottom 20";
 }
